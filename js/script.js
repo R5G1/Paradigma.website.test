cfa6418420fcf42975bf16fb0a2036a17e397a32
R5G1 <vlad.klimachevski@gmail.com>
2023-02-25 01:36:43 +0300

    feat: add basic page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-window.addEventListener('scroll', e => {
-    const scroll = document.querySelector(".header-navigation")
+// window.addEventListener('scroll', e => {
+//     const scroll = document.querySelector(".header-navigation")
 
-    if (pageYOffset > 500) {
-        scroll.classList.add('activ')
-    } if (pageYOffset < 500) {
-        scroll.classList.remove('activ')
-    }
-})
+//     if (pageYOffset > 500) {
+//         scroll.classList.add('activ')
+//     } if (pageYOffset < 500) {
+//         scroll.classList.remove('activ')
+//     }
+// })
+
+
+function showMap () {
+    YMaps.load(function() {
+        let mapContainer = YMaps.jQuery("#YMapsID"),
+            map = new YMaps.Map(mapContainer[0]);
+
+        mapContainer.css("display", "");
+        map.setCenter(new YMaps.GeoPoint(37.64, 55.76));
+    });
+}
